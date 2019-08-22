f2adb320982f827a59cb1e816cf8aed08fc7b5fc
Timu5 <timusziomek@gmail.com>
2019-08-22 19:52:46 +0200

    Add screens with parameters drawing

--- VAGTrip.ino
+++ VAGTrip.ino
@@ -6,6 +6,11 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
+char * names[] = { "Average Consumption", "Instant Consumption", "Trip Distance", "Trip Time" };
+float values[] = { 0, 0, 0, 0 };
+
+int sel = 0;
+
 void setup()
 {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
@@ -15,4 +20,20 @@ void setup()
 
 void loop()
 {
+  display.clearDisplay();
+	
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(names[sel]);
+  
+  display.drawLine(0, 20, 127, 20, WHITE);
+  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.println(values[sel]);
+ 
+  display.display();
+  delay(1);
 }
